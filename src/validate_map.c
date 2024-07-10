99b96f92060c4f3d8412f6e6cb8f2324092bd2f5
Tommy Rischmann <trischma@made-f0Cr11s3.clusters.42paris.fr>
2024-07-10 17:12:20 +0200

    Push automatique :)

--- src/validate_map.c
+++ src/validate_map.c
@@ -9,37 +9,26 @@ int	is_valid(int x, int y, t_map *map, int **visited)
 	return (1);
 }
 
-int	dfs(int x, int y, t_map *map, int **visited, char target)
+int	dfs(int x, int y, t_map *map, int **visited, char target, int *collectibles)
 {
 	if (map->map[y][x] == target)
 		return (1);
+	if (map->map[y][x] == 'C')
+		(*collectibles)++;
 	visited[y][x] = 1;
-	if (is_valid(x + 1, y, map, visited) && dfs(x + 1, y, map, visited, target))
+	if (is_valid(x + 1, y, map, visited) && dfs(x + 1, y,
+			map, visited, target, collectibles))
 		return (1);
-	if (is_valid(x - 1, y, map, visited) && dfs(x - 1, y, map, visited, target))
+	if (is_valid(x - 1, y, map, visited) && dfs(x - 1, y,
+			map, visited, target, collectibles))
 		return (1);
-	if (is_valid(x, y + 1, map, visited) && dfs(x, y + 1, map, visited, target))
+	if (is_valid(x, y + 1, map, visited) && dfs(x, y + 1,
+			map, visited, target, collectibles))
 		return (1);
-	if (is_valid(x, y - 1, map, visited) && dfs(x, y - 1, map, visited, target))
+	if (is_valid(x, y - 1, map, visited) && dfs(x, y - 1,
+			map, visited, target, collectibles))
 		return (1);
 	return (0);
-}
-
-int	dfs_collectibles(int x, int y, t_map *map, int **visited)
-{
-	int	count;
-
-	count = 0;
-	if (!is_valid(x, y, map, visited))
-		return (0);
-	if (map->map[y][x] == 'C')
-		count = 1;
-	visited[y][x] = 1;
-	count += dfs_collectibles(x + 1, y, map, visited);
-	count += dfs_collectibles(x - 1, y, map, visited);
-	count += dfs_collectibles(x, y + 1, map, visited);
-	count += dfs_collectibles(x, y - 1, map, visited);
-	return (count);
 }
 
 int	**allocate_visited(int height, int width)
@@ -79,19 +68,21 @@ void	validate_map_playable(t_game *game, t_map *map)
 {
 	int	**visited;
 	int	i;
-	int	reachable_collectibles;
+	int	collectibles_count;
 
 	visited = allocate_visited(map->height, map->width);
 	initialize_visited(visited, map->height, map->width);
+	collectibles_count = 0;
 	if (game->total_collectables > 0)
 	{
-		reachable_collectibles = dfs_collectibles(game->player_x,
-				game->player_y, map, visited);
-		if (reachable_collectibles != game->total_collectables)
+		dfs(game->player_x, game->player_y, map, visited,
+			' ', &collectibles_count);
+		if (collectibles_count != game->total_collectables)
 			error_exit("Not all collectibles are reachable");
 	}
 	initialize_visited(visited, map->height, map->width);
-	if (!dfs(game->player_x, game->player_y, map, visited, 'E'))
+	if (!dfs(game->player_x, game->player_y, map, visited,
+			'E', &collectibles_count))
 		error_exit("No valid path from P to E");
 	i = 0;
 	while (i < map->height)
