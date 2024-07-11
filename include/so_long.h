86b1b0bca6efa12c2a2428529bb2a8875ede8b00
Tommy Rischmann <trischma@made-f0Cr11s1.clusters.42paris.fr>
2024-07-11 16:00:06 +0200

    Push automatique :)

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: trischma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 14:54:44 by trischma          #+#    #+#             */
-/*   Updated: 2024/07/11 15:57:44 by trischma         ###   ########.fr       */
+/*   Updated: 2024/07/11 16:00:01 by trischma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,6 +108,6 @@ int		key_press(int keycode, t_game *game);
 void	validate_map_border(t_map *map);
 void	validate_map_playable(t_game *game, t_map *map);
 void	free_visited(int **visited, int height);
-void free_game_resources(t_game *game);
+void	free_game_resources(t_game *game);
 
 #endif
